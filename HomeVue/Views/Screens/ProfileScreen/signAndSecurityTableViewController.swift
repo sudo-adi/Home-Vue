ea0844119728d94564b5d1f444a927c443e38d14
Nishtha <dr.mac@Nishthas-mac.local>
2025-01-23 05:03:21 +0530

    Add .gitignore file

--- HomeVue/Views/Screens/ProfileScreen/signAndSecurityTableViewController.swift
+++ HomeVue/Views/Screens/ProfileScreen/signAndSecurityTableViewController.swift
@@ -6,100 +6,94 @@
 //
 
 import UIKit
-class signAndSecurityTableViewController: UITableViewController {
+class signAndSecurityTableViewController: UITableViewController,EditMailAndPhoneDelegate {
     
     @IBOutlet weak var EditEmailOrPhoneButton: UIButton!
     @IBOutlet weak var EmailLAbel: UILabel!
-    
     @IBOutlet weak var MobileNumberLabel: UILabel!
     @IBOutlet weak var ChangePassword: UIButton!
+    
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+            super.viewWillAppear(animated)
             
-        if let navigationBar = self.navigationController?.navigationBar {
+            // Update navigation bar appearance
+            if let navigationBar = self.navigationController?.navigationBar {
+                navigationBar.titleTextAttributes = [.foregroundColor: UIColor.gradientEndColor]
+                navigationBar.tintColor = UIColor.gradientEndColor
+            }
             
-            navigationBar.titleTextAttributes = [ .foregroundColor: UIColor.gradientEndColor]
-            navigationBar.tintColor = UIColor.gradientEndColor
+            // Fetch updated user data
+            updateUserInterface()
         }
-        
-    }
 
-    override func viewWillDisappear(_ animated: Bool) {
+        override func viewWillDisappear(_ animated: Bool) {
             super.viewWillDisappear(animated)
             
-        if let navigationBar = self.navigationController?.navigationBar {
-            // Revert to the default appearance
-            navigationBar.titleTextAttributes = nil
-            navigationBar.tintColor = nil
-        }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .solidBackgroundColor
-        EditEmailOrPhoneButton.addTarget(self, action: #selector(handleButtonTap(_:)), for: .touchUpInside)
-        ChangePassword.addTarget(self, action: #selector(handleButtonTap(_:)), for: .touchUpInside)
-        MobileNumberLabel.text = User1.phoneNumber
-        EmailLAbel.text = User1.email
-
-        self.view.applyGradientBackground()
-        
-       
-    }
-    
-    
-    func applyGradientBackground(to view: UIView, startColor: UIColor, endColor: UIColor) {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
-        gradientLayer.startPoint = CGPoint(x: 0, y: 0) // Top-left
-        gradientLayer.endPoint = CGPoint(x: 1, y: 1)   // Bottom-right
-        
-        // Add the gradient layer to the view
-        view.layer.insertSublayer(gradientLayer, at: 0)
-    }
-
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-            if let headerView = view as? UITableViewHeaderFooterView {
-                headerView.textLabel?.textColor = .white
-                headerView.textLabel?.font = UIFont.headerFont()
+            // Reset navigation bar appearance
+            if let navigationBar = self.navigationController?.navigationBar {
+                navigationBar.titleTextAttributes = nil
+                navigationBar.tintColor = nil
             }
         }
-    
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.backgroundColor = .solidBackgroundColor
+            
+            // Set up button actions
+            EditEmailOrPhoneButton.addTarget(self, action: #selector(handleButtonTap(_:)), for: .touchUpInside)
+            ChangePassword.addTarget(self, action: #selector(handleButtonTap(_:)), for: .touchUpInside)
+            
+            // Apply gradient background
+            self.view.applyGradientBackground()
+            
+            // Fetch and display user data
+            updateUserInterface()
+        }
+
+        func updateUserInterface() {
+            EmailLAbel.text = User1.email
+            MobileNumberLabel.text = User1.phoneNumber
+        }
+
+//    @IBOutlet weak var ChangePassword: UIButton! // Ensure this is connected in the storyboard
+
     @objc func handleButtonTap(_ sender: UIButton) {
-            let alertController = UIAlertController(title: "Verify Password", message: "Enter your current password", preferredStyle: .alert)
-            
-            alertController.addTextField { textField in
-                textField.placeholder = "Current Password"
-                textField.isSecureTextEntry = true
-            }
-        let UserPassword = User1.password
- 
-            let verifyAction = UIAlertAction(title: "Verify", style: .default) { _ in
-                if let password = alertController.textFields?.first?.text, password == UserPassword {
-                    if sender == self.EditEmailOrPhoneButton {
-                        self.performSegue(withIdentifier: "EditEmailSegue", sender: nil)
-                    } else if sender == self.ChangePassword {
-                        self.performSegue(withIdentifier: "ChangePasswordSegue", sender: nil)
-                    }
-                } else {
-                    self.showErrorMessage()
-                }
-            }
-            
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-            
-            alertController.addAction(verifyAction)
-            alertController.addAction(cancelAction)
-            
-            self.present(alertController, animated: true, completion: nil)
+        let alertController = UIAlertController(title: "Verify Password", message: "Enter your current password", preferredStyle: .alert)
+        
+        alertController.addTextField { textField in
+            textField.placeholder = "Current Password"
+            textField.isSecureTextEntry = true
         }
+        
+        let userPassword = User1.password
+        
+        let verifyAction = UIAlertAction(title: "Verify", style: .default) { _ in
+            if let password = alertController.textFields?.first?.text, password == userPassword {
+                if sender == self.EditEmailOrPhoneButton {
+                    self.performSegue(withIdentifier: "EditEmailSegue", sender: nil)
+                } else if sender == self.ChangePassword { // Use the correct outlet name
+                    self.performSegue(withIdentifier: "ChangePasswordSegue", sender: nil)
+                }
+            } else {
+                self.showErrorMessage()
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(verifyAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+        if sender == self.EditEmailOrPhoneButton {
+            performSegue(withIdentifier: "EditEmailSegue", sender: nil)
+        } else if sender == self.ChangePassword {
+            performSegue(withIdentifier: "ChangePasswordSegue", sender: nil)
+        }
+    }
+
         
         func showErrorMessage() {
             let errorAlert = UIAlertController(title: "Error", message: "Incorrect password. Please try again.", preferredStyle: .alert)
@@ -107,8 +101,36 @@ class signAndSecurityTableViewController: UITableViewController {
             self.present(errorAlert, animated: true, completion: nil)
         }
 
-    @IBAction func unwindToSignIn(_ segue: UIStoryboardSegue) {
-        // Perform any cleanup or setup after unwinding here
-        print("Unwind to Sign In triggered")
-    }
+    
+    
+    func didUpdateEmailAndPhone(email: String, phone: String) {
+            User1.email = email
+            User1.phoneNumber = phone
+            
+            // Update the UI with the new values
+            EmailLAbel.text = email
+            MobileNumberLabel.text = phone
+        }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "EditEmailSegue" {
+                if let destinationVC = segue.destination as? EditMailAndPhoneTableViewController {
+                    destinationVC.delegate = self
+                }
+            }
+        }
+    
+    
+//    @IBAction func unwindToLoginMainPage(_ segue: UIStoryboardSegue) {
+//        // Update the main page UI if needed
+//        EmailLAbel.text = User1.email
+//        MobileNumberLabel.text = User1.phoneNumber
+//    }
+//    @IBAction func unwindToLoginMainPage(_ segue: UIStoryboardSegue) {
+//        // Update the main page UI if needed
+//        personEmail.text = User1.email
+//        personPhone.text = User1.phoneNumber
+//    }
+
+
 }
