f71ff8a3daad37b33a69e6791534bf5ca166b08f
Bhumi-1020 <bhumi226.be22@chitkara.edu.in>
2025-03-26 12:30:31 +0530

    removed static data of room models.

--- HomeVue/Views/Screens/RoomCollectionView/RoomsCollectionViewCell.swift
+++ HomeVue/Views/Screens/RoomCollectionView/RoomsCollectionViewCell.swift
@@ -12,29 +12,6 @@ class RoomsCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var dateCreatedLabel: UILabel!
     
-    func configure(with room: RoomModel) {
-        nameLabel.text = room.details.name
-        dateCreatedLabel.text = "Created on: \(room.details.createdDate.formatted())"
-        
-        // Generate thumbnail from 3D model
-        if let modelPath = room.details.model3D,
-           let modelURL = URL(string: modelPath) {
-            DispatchQueue.global(qos: .background).async {
-                if let thumbnail = ThumbnailGenerator.generateThumbnail(from: modelURL) {
-                    DispatchQueue.main.async {
-                        self.roomImage.image = thumbnail
-                    }
-                } else {
-                    DispatchQueue.main.async {
-                        self.roomImage.image = UIImage(named: "model_img")
-                    }
-                }
-            }
-        } else {
-            roomImage.image = UIImage(named: "model_img")
-        }
-    }
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         self.layer.borderColor = UIColor.lightGray.cgColor
@@ -42,11 +19,4 @@ class RoomsCollectionViewCell: UICollectionViewCell {
         self.addCornerRadius(10)
     }
       
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        roomImage.image = UIImage(named: "model_img")
-        nameLabel.text = nil
-        dateCreatedLabel.text = nil
-    }
-        
 }
